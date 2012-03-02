0dd5c79537661c74add0905196945a7716055b74
Mike Ryan <mike@fadedink.co.uk>
2012-03-02 14:16:56 +0000

    fixed content types when saving to S3

--- s3cacher.py
+++ s3cacher.py
@@ -19,6 +19,7 @@ def cache_image(path):
             return '404 NOT FOUND', False, ''
         raise
     image_data = StringIO(image_file.read())
+    content_type = image_file.info()['content-type']
     print 'Download complete: %s' % url
 
     # connect to S3 and save the resized image to the target bucket
@@ -28,12 +29,12 @@ def cache_image(path):
     k = Key(bucket)
     k.key = path
     print 'Saving to S3: %s' % path
+    k.content_type = content_type
     k.set_contents_from_file(image_data)
     k.set_acl('public-read')
     print 'Save to S3 complete: %s' % path
 
-    # TODO send proper headers
-    headers = [('Content-type', image_file.info()['content-type'])]
+    headers = [('Content-type', content_type)]
     return '200 OK', headers, image_data.getvalue()
 
 def app(environ, start_response):
